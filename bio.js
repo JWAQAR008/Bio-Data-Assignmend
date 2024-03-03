8dabccf51f3a1909b1821fb51af875608c8eacf4
JWAQAR008 <160424894+JWAQAR008@users.noreply.github.com>
2024-03-03 10:37:52 -0800

    first commit

---
+++ bio.js
@@ -0,0 +1,119 @@
+//MY_NAME
+var firstName = "waqar";
+console.log(firstName);
+var lastname = "hussaim";
+console.log(lastname);
+//MY Fathername
+var fathername = "Abdul_Razzaque";
+console.log(fathername);
+//My Cast
+var cast = "Arain";
+console.log(cast);
+//MyAge
+var age = 19;
+age = 20;
+console.log(age);
+//My height
+var My_height = "5.7";
+My_height = "5.8";
+console.log(My_height);
+// My Weight
+var Weight = 56;
+Weight = 57;
+console.log(Weight);
+//MY nickname
+var nickname = "Qari";
+nickname = "vicky";
+console.log(nickname);
+//MY Date of Birth
+var dateofbirth = "02.28.2002";
+dateofbirth = "28.02.2002";
+console.log(dateofbirth);
+//my school
+var school = "NEW City Public High School";
+school = "NEW City Public High School Golarchi";
+console.log(school);
+//My Collage
+var Collage = "Govt Boys higher secondery school & collage";
+Collage = "Govt Boys higher secondery collage";
+console.log(Collage);
+// MY City Name
+var cityname = "Golarchi Saheed Fazal Raho";
+cityname = "Golarchi";
+//My Distric Name
+var Districname = "Thatta";
+Districname = "badin";
+console.log(Districname);
+// My province name 
+var province_Name = "Punjab";
+province_Name = "sindh";
+console.log(province_Name);
+// My country name 
+var country = "Islamic Republic of Pakistan";
+country = "pakistan";
+console.log(country);
+//My Nationality
+var nationality = "Dubai";
+nationality = "Pakistani";
+console.log(nationality);
+//My Language
+var language = "english";
+language = "Urdu";
+console.log(language);
+//My religion
+var religion = "muslim";
+religion = "muslim";
+console.log(religion);
+//MY Residence
+var residence = "i live in jamshoro";
+residence = 'I live in noriabad';
+console.log(residence);
+//My experience
+var experience = "winder operater";
+experience = "slitter operater";
+console.log(experience);
+// my goal
+var goal = "to be a webdevoloper.";
+console.log(goal);
+//my course
+var course = "AI";
+course = "Artificaial intelligence";
+console.log(course);
+// Education
+var educated = false;
+educated = true;
+console.log(educated);
+var educatiom = "intermediate in medical";
+educatiom = "Intermediate in engineering";
+console.log(educatiom);
+//My Matric Result
+var matriculation_result = "fail";
+matriculation_result = "pass";
+console.log(matriculation_result);
+//My Inter Result
+var intermediate_result = "fail";
+intermediate_result = "pass";
+console.log(intermediate_result);
+var matricpercentage = 69;
+console.log(matricpercentage);
+var interpercenatge = 68;
+console.log(interpercenatge);
+//My intrest
+var intrest = "gaming";
+intrest = "coding";
+console.log(intrest);
+//my skills
+var skill = "HTML/CSS";
+console.log(skill);
+//learning skill
+var learningskill = "typescript";
+console.log(learningskill);
+var myclasstiming = "monday 02pm.to 5pm.";
+console.log(myclasstiming);
+var myclassduration = "3 hours";
+console.log(myclassduration);
+//contact detail
+var sellnumber = (92 + 3420039786);
+console.log(sellnumber);
+var emailaddres = "jwaqar008@gmail.com";
+console.log(emailaddres);
---
+++ bio.ts
@@ -0,0 +1,155 @@
+//MY_NAME
+let  firstName: string = "waqar"
+console.log(firstName);
+
+let lastname: string = "hussaim"
+console.log(lastname);
+
+//MY Fathername
+ let fathername : string = "Abdul_Razzaque"
+ console.log(fathername);
+
+//My Cast
+ let cast: string= "Arain"
+ console.log(cast);
+
+//MyAge
+ let age : number= 19;
+ age = 20;
+ console.log(age);
+
+ //My height
+ let  My_height: string = "5.7"
+My_height = "5.8";
+console.log(My_height);
+
+// My Weight
+let Weight : Number = 56;
+Weight = 57;
+console.log(Weight);
+
+//MY nickname
+let nickname: string = "Qari"
+nickname = "vicky"
+console.log(nickname);
+
+//MY Date of Birth
+let dateofbirth : string = "date of birth";
+dateofbirth = "28.02.2002"
+console.log(dateofbirth);
+ 
+//my school
+let school : string = "NEW City Public High School"
+school = "NEW City Public High School Golarchi"
+console.log(school);
+
+//My Collage
+let Collage : string = "Govt Boys higher secondery school & collage"
+Collage = "Govt Boys higher secondery collage"
+console.log(Collage);
+
+ // MY City Name
+  let cityname : string = "Golarchi Saheed Fazal Raho"
+cityname = "Golarchi"
+
+//My Distric Name
+let Districname : string = "Thatta"
+Districname = "badin"
+console.log(Districname);
+
+// My province name 
+let  province_Name :string = "Punjab"
+province_Name = "sindh"
+console.log(province_Name);
+
+// My country name 
+ let country : string = "Islamic Republic of Pakistan"
+ country = "pakistan"
+ console.log(country);
+
+ //My Nationality
+ let nationality: string ="Dubai"
+ nationality="Pakistani";
+ console.log(nationality);
+
+ //My Language
+ let language : string= "english";
+ language = "Urdu";
+ console.log(language);
+
+ //My religion
+ let religion: string = "muslim"
+ religion = "muslim"
+console.log(religion);
+
+//MY Residence
+let residence : string = "i live in jamshoro";
+residence = 'I live in noriabad';
+console.log(residence);
+
+//My experience
+let experience  : string = "winder operater";
+experience = "slitter operater";
+console.log(experience);
+
+// my goal
+let goal : string = "to be a webdevoloper.";
+console.log(goal);
+
+//my course
+let course : string = "AI"
+course = "Artificaial intelligence";
+console.log(course);
+
+// Education
+let educated : boolean = false;
+educated = true;
+console.log(educated)
+
+let educatiom : string = "intermediate in medical";
+educatiom =  "Intermediate in engineering"
+console.log(educatiom);
+ 
+//My Matric Result
+let matriculation_result : string = "fail";
+matriculation_result = "pass";
+console.log(matriculation_result);
+ 
+//My Inter Result
+let intermediate_result : string = "fail";
+intermediate_result = "pass";
+console.log(intermediate_result);
+let matricpercentage  : number= 69;
+console.log(matricpercentage);
+let interpercenatge    : number= 68.18;
+console.log(interpercenatge);
+
+//My intrest
+let intrest: string = "gaming";
+intrest = "coding";
+console.log(intrest);
+
+//my skills
+let skill : string = "HTML/CSS";
+console.log(skill);
+
+//learning skill
+let learningskill : string = "typescript";
+console.log(learningskill);
+   
+let myclasstiming : string = "monday 02pm.to 5pm.";
+console.log(myclasstiming);
+
+let myclassduration : string = "3 hours";
+console.log(myclassduration);
+
+//contact detail
+let sellnumber: number =(92+3420039786);
+ console.log(sellnumber);
+
+let emailaddres :string ="jwaqar008@gmail.com" ;
+console.log(emailaddres);
+
+
+
+
